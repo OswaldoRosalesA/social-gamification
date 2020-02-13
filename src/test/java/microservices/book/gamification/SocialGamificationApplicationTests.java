4438c95920de5b1fcf77f6255edb9a32769f8b6c
Oswaldo Rosales <oswaldo.rosales.a@gmail.com>
2020-02-13 10:01:00 -0600

    Springboot base project files

---
+++ src/test/java/microservices/book/gamification/SocialGamificationApplicationTests.java
@@ -0,0 +1,13 @@
+package microservices.book.gamification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialGamificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
