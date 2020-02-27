2aa158aea5684ad79f0aeb6aaa02d8315c5f258f
Oswaldo Rosales <oswaldo.rosales.a@gmail.com>
2020-02-27 09:17:39 -0600

    Feature: Chapter 5 - Discovery Client

--- src/main/java/microservices/book/gamification/SocialGamificationApplication.java
+++ src/main/java/microservices/book/gamification/SocialGamificationApplication.java
@@ -2,7 +2,9 @@ package microservices.book.gamification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SocialGamificationApplication {
 
