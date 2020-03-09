a3bd1779215bd5f1ef8b84bab38461691c8b8e64
Oswaldo Rosales <oswaldo.rosales.a@gmail.com>
2020-03-09 09:49:54 -0600

    Feature: Chapter 5 - Hystrix

--- src/main/java/microservices/book/gamification/client/MultiplicationResultAttemptClientImpl.java
+++ src/main/java/microservices/book/gamification/client/MultiplicationResultAttemptClientImpl.java
@@ -1,5 +1,6 @@
 package microservices.book.gamification.client;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import microservices.book.gamification.client.dto.MultiplicationResultAttempt;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -17,6 +18,7 @@ public class MultiplicationResultAttemptClientImpl implements MultiplicationResu
         this.multiplicationHost = multiplicationHost;
     }
 
+    @HystrixCommand(fallbackMethod = "defaultResult")
     @Override
     public MultiplicationResultAttempt retrieveMultiplicationResultAttemptbyId(Long multiplicationId) {
         StringBuilder stringBuilder = new StringBuilder();
@@ -24,5 +26,10 @@ public class MultiplicationResultAttemptClientImpl implements MultiplicationResu
         stringBuilder.append("/results/");
         stringBuilder.append(multiplicationId);
         return restTemplate.getForObject(stringBuilder.toString(), MultiplicationResultAttempt.class);
+    }
+
+    private MultiplicationResultAttempt defaultResult(final Long multiplicationResultAttemptId) {
+        return new MultiplicationResultAttempt("fakeAlias",
+                10, 10, 100, true);
     }
 }
